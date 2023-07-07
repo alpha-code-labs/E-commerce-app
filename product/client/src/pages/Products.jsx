8845baf56fbe9b2f1335f52517f00c136cbf372a
Anandhu <anandhuashok.k@gmail.com>
2023-07-07 12:31:14 +0530

    product frontend added

---
+++ product/client/src/pages/Products.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import DisplayDefaultProducts from '../components/DisplayDefaultProducts'
+import DisplaySearchedProducts from '../components/DisplaySearchedProducts'
+import axios from 'axios'
+
+
+export default function Products(props){
+    //change to const
+    var productList = props.productList
+
+  
+
+    const [defaultProductList, setDefaultProducts] = useState(null) 
+    const productListUrl = ''
+    const products = [{category:'Fashion', name:'color eye lens'}, {category:'electronics and stuff', name:'Refrigerator'}, {category:'fashion', name:'Maskara'}, {category:'electronics', name:'Air Congitioner'}, {category:'fashion', name:'Lipstick'}, {category:'sports', name:'Badminton Racket'}]
+
+    //temporary arrangement
+     //const productList = products
+
+    useEffect(()=>{
+        if(!productList){
+       
+            /*
+            axios.get(productListUrl).then((response)=>{
+                setDefaultProducts(response.data)
+            })
+        */
+
+            //temporary arrangement
+            setDefaultProducts(products)
+        }
+
+        
+
+    },[])
+
+
+    return(
+    <>
+        {productList && <DisplaySearchedProducts products={productList}/>}
+        {defaultProductList && <DisplayDefaultProducts products={defaultProductList} />}
+    </>
+    )
+}
